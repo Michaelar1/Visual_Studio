2a3c0d6abbe4297c44c499d9d5a84f2b9c9aa3b0
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-03-29 18:00:48 -0400

    Created VisualStudioHTMLSubmissionAssignment

---
+++ VisualStudioHTMLSubmissionAssignment/VisualStudioHTMLSubmissionAssignment/wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+//  Animation code -- I could have probably made this cooler with @keyframes in CSS but this should satisfy the requirement of the assignment.
+function danse() {
+    let id = null;
+    const elem = document.getElementById("animate");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
